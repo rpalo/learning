e05976e74b855aed025f93f82184c46f3d3cef9f
Ryan Palo <ryan@thepalos.com>
2025-06-08 08:16:48 +0000

    Basic interpreter

---
+++ bf/bf/bf.go
@@ -0,0 +1,58 @@
+package bf
+
+import (
+	"fmt"
+	"log"
+)
+
+func EvalBf(source string, buffer_size int, debug bool) {
+	i := 0
+	d := 0
+	loopCounter := 0
+	buffer := make([]int, buffer_size)
+
+	for i >= 0 && i < len(source) {
+		if debug {
+			fmt.Printf("%d: %c, %d: [%d]\n", i, source[i], d, buffer[d])
+		}
+		switch source[i] {
+		case '>':
+			d = (d + 1) % buffer_size
+		case '<':
+			d = (d - 1) % buffer_size
+		case '+':
+			buffer[d]++
+		case '-':
+			buffer[d]--
+		case '.':
+			fmt.Printf("%c", buffer[d])
+		case ',':
+			_, err := fmt.Scanf("%c", &buffer[d])
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		case '[':
+			if buffer[d] == 0 {
+				for ; source[i] != ']' || loopCounter != 0; i++ {
+					if source[i] == '[' {
+						loopCounter++
+					} else if source[i] == ']' {
+						loopCounter--
+					}
+				}
+			}
+		case ']':
+			if buffer[d] != 0 {
+				for ; source[i] != '[' || loopCounter != 0; i-- {
+					if source[i] == '[' {
+						loopCounter--
+					} else if source[i] == ']' {
+						loopCounter++
+					}
+				}
+			}
+		}
+		i++
+	}
+}
--- bf/main.go
+++ bf/main.go
@@ -7,11 +7,52 @@ import (
 	"io"
 	"log"
 	"os"
+	"slices"
+	"strconv"
+
+	"github.com/rpalo/learning/bf/bf"
 )
 
-func main() {
-	repl()
+const USAGE = `
+usage: bf [-h|FILENAME]
 
+Evaluate brainf*ck.  If FILENAME is provided, run that.  Otherwise run a REPL.
+`
+
+var buffer_size = 30000
+var debug = false
+
+func init() {
+	if val := os.Getenv("BF_BUFFER_SIZE"); val != "" {
+		size, err := strconv.Atoi(val)
+
+		if err != nil {
+			log.Fatalf("Env var BF_BUFFER_SIZE is not an integer: %s", val)
+		}
+		buffer_size = size
+	}
+	if os.Getenv("BF_DEBUG") != "" {
+		debug = true
+	}
+}
+
+func main() {
+	if slices.Contains(os.Args, "-h") {
+		fmt.Print(USAGE)
+		return
+	}
+
+	if len(os.Args) == 2 {
+		contents, err := os.ReadFile(os.Args[1])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bf.EvalBf(string(contents), buffer_size, debug)
+	} else {
+		repl()
+	}
 }
 
 func repl() {
@@ -32,6 +73,6 @@ func repl() {
 		if len(line) == 0 {
 			return
 		}
-		fmt.Println(line)
+		bf.EvalBf(line, buffer_size, debug)
 	}
 }
