a7d165a2aa44084124e408cdc0a9b0db28eb2d82
Ryan Palo <ryan@thepalos.com>
2025-06-11 18:10:30 +0000

    Parse string key/val pairs

--- jr/lexer.go
+++ jr/lexer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type ErrLex struct {
@@ -14,11 +15,23 @@ func (e ErrLex) Error() string {
 	return fmt.Sprintf("Unrecognized character: %s", e.text)
 }
 
-func Lex(reader io.Reader) ([]string, error) {
+type TokenType int
+
+const (
+	TokenString TokenType = 1
+	TokenRaw              = 2
+)
+
+type Token struct {
+	kind  TokenType
+	value string
+}
+
+func Lex(reader io.Reader) ([]Token, error) {
 	scanner := bufio.NewScanner(reader)
 	scanner.Split(bufio.ScanRunes)
 
-	result := make([]string, 0)
+	result := make([]Token, 0)
 
 	for {
 		more := scanner.Scan()
@@ -28,10 +41,31 @@ func Lex(reader io.Reader) ([]string, error) {
 		c := scanner.Text()
 
 		switch c {
-		case "{", "}":
-			result = append(result, c)
+		case "{", "}", ":", ",":
+			result = append(result, Token{kind: TokenRaw, value: c})
+		case " ", "\n", "\t":
+			// ignore whitespace (note could be more robust)
+		case "\"":
+			text, err := lexString(scanner)
+
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, Token{kind: TokenString, value: text})
 		default:
 			return nil, ErrLex{c}
 		}
 	}
+}
+
+func lexString(scanner *bufio.Scanner) (string, error) {
+	chars := make([]string, 0)
+
+	for scanner.Scan() {
+		if scanner.Text() == "\"" && (len(chars) == 0 || chars[len(chars)-1] != "\\") {
+			return strings.Join(chars, ""), nil
+		}
+		chars = append(chars, scanner.Text())
+	}
+	return "", ErrLex{"Unterminated string"}
 }
--- jr/parser.go
+++ jr/parser.go
@@ -1,41 +1,130 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type ErrParse struct {
-	reason string
-	token  string
+	reason   string
+	expected string
+	actual   string
 }
 
 func (e ErrParse) Error() string {
-	return fmt.Sprintf("Error parsing: %s", e.token)
+	return fmt.Sprintf("Error parsing: %s: %s, got %s", e.reason, e.expected, e.actual)
 }
 
-type JsonObject struct{}
+type JsonObject map[string]string
 
-func (o *JsonObject) String() string {
-	return "{}"
+func (o JsonObject) String() string {
+	result := []string{"{"}
+	for key, value := range o {
+		result = append(result, fmt.Sprintf("\t%s: %s,", key, value))
+	}
+	result = append(result, "}")
+	return strings.Join(result, "\n")
 }
 
-func Parse(tokens []string) (*JsonObject, error) {
-	return parseObject(tokens)
+func Parse(tokens []Token) (JsonObject, error) {
+	result, tokens, err := parseObject(tokens)
+	if err != nil {
+		return nil, err
+	}
+	if len(tokens) > 0 {
+		return nil, ErrParse{"Extra tokens after object.", "", tokens[0].value}
+	}
+	return result, nil
 }
 
-func parseObject(tokens []string) (*JsonObject, error) {
+func parseObject(tokens []Token) (JsonObject, []Token, error) {
+	if len(tokens) < 2 {
+		return nil, nil, ErrParse{"Not enough tokens to parse object.", "", ""}
+	}
+
+	if _, err := expectRaw(tokens, "{"); err != nil {
+		return nil, nil, err
+	}
+
+	result := make(map[string]string)
+
+	if _, err := expectRaw(tokens[1:], "}"); err == nil {
+		remaining, err := advance(tokens, 2)
+		return result, remaining, err
+	} else {
+		tokens, _ = advance(tokens, 1)
+	}
+
+	for {
+		if len(tokens) < 4 {
+			return nil, nil, ErrParse{"Not enough tokens for key-value pair.", "", ""}
+		}
+
+		key, err := expectString(tokens)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if _, err := expectRaw(tokens[1:], ":"); err != nil {
+			return nil, nil, err
+		}
+
+		value, err := expectString(tokens[2:])
+
+		if err != nil {
+			return nil, nil, err
+		}
+		result[key] = value
+
+		if _, err := expectRaw(tokens[3:], "}"); err == nil {
+			tokens = tokens[3:]
+			break
+		}
+
+		if _, err := expectRaw(tokens[3:], ","); err != nil {
+			return nil, nil, err
+		}
+
+		tokens, _ = advance(tokens, 4)
+	}
+
+	if _, err := expectRaw(tokens, "}"); err != nil {
+		return nil, nil, err
+	}
+	remaining, err := advance(tokens, 1)
+	return result, remaining, err
+}
+
+func advance(tokens []Token, count int) ([]Token, error) {
+	if len(tokens) < count {
+		return nil, ErrParse{"Not enough tokens to advance.", "", ""}
+	}
+	if len(tokens) == count {
+		return nil, nil // No tokens left to advance
+	}
+	return tokens[count:], nil
+}
+
+func expectRaw(tokens []Token, value string) (string, error) {
 	if len(tokens) == 0 {
-		return nil, ErrParse{"Empty file.", ""}
+		return "", ErrParse{"Missing expected token.", value, ""}
 	}
-	if tokens[0] != "{" {
-		return nil, ErrParse{"Unexpected token", tokens[0]}
+	if tokens[0].kind != TokenRaw {
+		return "", ErrParse{"Expected raw value.", value, tokens[0].value}
 	}
-	if len(tokens) == 1 {
-		return nil, ErrParse{"Unclosed object.", ""}
+	if tokens[0].value != value {
+		return "", ErrParse{"Unexpected token.", value, tokens[0].value}
 	}
-	if tokens[1] != "}" {
-		return nil, ErrParse{"Unexpected token", tokens[1]}
+	return tokens[0].value, nil
+}
+
+func expectString(tokens []Token) (string, error) {
+	if len(tokens) == 0 {
+		return "", ErrParse{"Missing expected string.", "string", ""}
 	}
-	if len(tokens) > 2 {
-		return nil, ErrParse{"Extra token", tokens[2]}
+	if tokens[0].kind != TokenString {
+		return "", ErrParse{"Expected string.", "string", tokens[0].value}
 	}
-	return &JsonObject{}, nil
+	return tokens[0].value, nil
 }
